4cc94126599ede92c7182fc94029ea6f94de093d
Jheniffer Araujo <jhenifferbaldi@gmail.com>
2021-09-09 11:07:33 -0300

    Change header background color on scroll

--- js/main.js
+++ js/main.js
@@ -3,14 +3,14 @@ const   navMenu = document.getElementById('nav-menu'),
         navToggle = document.getElementById('nav-toggle'),
         navClose = document.getElementById('nav-close')
 
-/*=============== MENU SHOW ===============*/ 
+/*====== MENU SHOW ======*/ 
 if(navToggle) {
     navToggle.addEventListener('click', () => {
         navMenu.classList.add('show-menu')
     })
 }
 
-/*=============== MENU HIDDEN ===============*/
+/*====== MENU HIDDEN ======*/
 if(navClose) {
     navClose.addEventListener('click', () => {
         navMenu.classList.remove('show-menu')
@@ -23,5 +23,12 @@ const navLink = document.querySelectorAll('.nav__link')
 function linkAction() {
     navMenu.classList.remove('show-menu')
 }
-
 navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*=============== CHANGE BACKGROUND HEADER ===============*/
+function scrollHeader() {
+    const header = document.getElementById('header')
+
+    if(this.scrollY >= 100) header.classList.add('scroll-header'); else header.classList.remove('scroll-header')
+}
+window.addEventListener('scroll', scrollHeader)
